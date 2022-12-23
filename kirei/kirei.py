36efeccae24fadcb8e4adb0937fb60da7e6eb062
brave24221 <brave24221@proton.me>
2022-12-22 21:36:03 -0500

    removing hard coded twitter list IDs in order to copy and paste twitter ID URLs

--- kirei/kirei.py
+++ kirei/kirei.py
@@ -16,15 +16,26 @@ client = tweepy.Client (
 
 def main():
     """Removing users with certain letters at the beginning of their username from a twitter list."""
-    twitter_lst = client.get_list_members(id="1602825510702268416", user_auth=True).data
-    name_of_lst = client.get_list(id="1602825510702268416", user_auth=True).data
+    print("Go to the main page of any of your specified twitter lists."
+    " The link at the top of the page should look something like this:"
+    " https://twitter.com/i/lists/series-of-numbers"
+    " Copy that link and submit it below.")
+
+    twitter_list_url = input("Paste the twitter list link here: ")
+
+    # Removing unneeded information from the URL in order to obtain the twitter ID.
+    stripped_twitter_list_url = int(twitter_list_url.split('/')[-1])
+    print(f"The name of your twitter list is \"{client.get_list(id=stripped_twitter_list_url, user_auth=True).data['name'].capitalize()}\".")
+
+    twitter_list = client.get_list_members(id=stripped_twitter_list_url, user_auth=True).data
+    name_of_lst = client.get_list(id=stripped_twitter_list_url, user_auth=True).data
 
     enter_beginning_of_username = input("Enter the first few letters of the specified person's username: ")
 
-    lst_of_usernames = [member.username for member in twitter_lst]
-    lst_of_ids = [member.id for member in twitter_lst]
+    lst_of_usernames = [member.username for member in twitter_list]
+    lst_of_ids = [member.id for member in twitter_list]
 
-    print(f'\n*** There are {len(twitter_lst)} users in this list. Below are all the users who usernames start with {enter_beginning_of_username.title()}. ***\n')
+    print(f'\n*** There are {len(twitter_list)} users in this list. Below are all the users who usernames start with {enter_beginning_of_username.title()}. ***\n')
     time.sleep(2)
     print('Press enter to continue')
     input()
@@ -34,7 +45,7 @@ def main():
 
         if member_username.lower().startswith(enter_beginning_of_username.lower()):
             print(f"Removing {member_username} | ID: {member_id} from the list titled \"{name_of_lst.data['name'].capitalize()}\"\n")
-            client.remove_list_member(id="1602825510702268416", user_id=member_id, user_auth=True)
+            client.remove_list_member(id=stripped_twitter_list_url, user_id=member_id, user_auth=True)
             time.sleep(0.3)
 
 
